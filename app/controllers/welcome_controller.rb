94e63e432b61a6070982438341b9dfbd55789c20
Florian Schaeffler <flo@hikey.io>
2016-10-04 23:57:21 +0300

    added favicon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
-  def index
-  	@server_info = ServerInfo.find_myself
-  	@other_server_infos = ServerInfo.where(["id != ?", @server_info.id])
-  end
+	def index
+		@server_info = ServerInfo.find_myself
+		@other_server_infos = ServerInfo.where(["id != ?", @server_info.id])
+	end
 
 	def send_email
 		ServerInfoMailer.send_email(server_info: ServerInfo.find_myself).deliver_later
