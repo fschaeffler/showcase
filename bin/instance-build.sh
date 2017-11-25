2df7d023053c6e7f6260f09a7dee43173f9889ac
Florian Schaeffler <flo@hikey.io>
2017-11-25 21:50:16 +0100

    added unattended upgrade

--- bin/instance-build.sh
+++ bin/instance-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 
 # ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
