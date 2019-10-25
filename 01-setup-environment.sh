f843fd6f10740909365ccf83071c5c53631d89fc
Florian Schaeffler <flo@hikey.io>
2019-10-25 07:04:40 +0200

    updated setup instructions

--- 01-setup-environment.sh
+++ 01-setup-environment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# install system dependencies
+sudo apt-get install git gnupg curl -y
+
 # install ruby via rvm
 gpg --keyserver hkp://keys.gnupg.net \
 	--recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
 rvm install "ruby-2.5.1"
 
@@ -18,6 +21,6 @@ export NVM_DIR="$HOME/.nvm"
 nvm install 8
 
 # prepare showcase-app
-git clone https://github.com/fschaeffler/showcase.git; cd showcase
 sudo apt-get install libmysqlclient-dev -y
+git clone https://github.com/fschaeffler/showcase.git; cd showcase
 bundle
